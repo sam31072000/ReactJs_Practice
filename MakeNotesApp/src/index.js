e3e32889e90d7d5c62b56affcb3013d1a9dfdf8f
Shaksham Agarwal <57569810+sam31072000@users.noreply.github.com>
2020-06-24 23:45:06 +0530

    Create index.js

---
+++ MakeNotesApp/src/index.js
@@ -0,0 +1,12 @@
+ import React from "react";
+ import ReactDOM from 'react-dom';
+ import './index.css';
+ import App from './App';
+
+//user defined tags start with capital letter nd inbuild start with small letter
+
+ ReactDOM.render(
+   <>
+     <App />
+   </>,document.getElementById("root")
+ );
